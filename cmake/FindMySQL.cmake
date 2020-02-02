39dbc4ea02ac562424e06f45daf8a16d4abfe2b8
vlad <harborwolf1@gmail.com>
2020-02-02 17:02:49 +0300

    ts

--- cmake/FindMySQL.cmake
+++ cmake/FindMySQL.cmake
@@ -1,79 +1,51 @@
-if(NOT CMAKE_CROSSCOMPILING)
-  find_program(MYSQL_CONFIG
-    NAMES mysql_config mariadb_config
-  )
-
-  if(MYSQL_CONFIG)
-    exec_program(${MYSQL_CONFIG}
-      ARGS --include
-      OUTPUT_VARIABLE MY_TMP
-    )
-
-    string(REGEX REPLACE "-I([^ ]*)( .*)?" "\\1" MY_TMP "${MY_TMP}")
-
-    set(MYSQL_CONFIG_INCLUDE_DIR ${MY_TMP} CACHE FILEPATH INTERNAL)
-
-    exec_program(${MYSQL_CONFIG}
-      ARGS --libs_r
-      OUTPUT_VARIABLE MY_TMP
-    )
-
-    set(MYSQL_CONFIG_LIBRARIES "")
-
-    string(REGEX MATCHALL "-l[^ ]*" MYSQL_LIB_LIST "${MY_TMP}")
-    foreach(LIB ${MYSQL_LIB_LIST})
-      string(REGEX REPLACE "[ ]*-l([^ ]*)" "\\1" LIB "${LIB}")
-      list(APPEND MYSQL_CONFIG_LIBRARIES "${LIB}")
-    endforeach()
-
-    set(MYSQL_CONFIG_LIBRARY_PATH "")
-
-    string(REGEX MATCHALL "-L[^ ]*" MYSQL_LIBDIR_LIST "${MY_TMP}")
-    foreach(LIB ${MYSQL_LIBDIR_LIST})
-      string(REGEX REPLACE "[ ]*-L([^ ]*)" "\\1" LIB "${LIB}")
-      list(APPEND MYSQL_CONFIG_LIBRARY_PATH "${LIB}")
-    endforeach()
-  endif()
-endif()
-
 set_extra_dirs_lib(MYSQL mysql)
-find_library(MYSQL_LIBRARY
+####################################### MySQL
+find_library(OWN_MYSQL_LIBRARY
   NAMES "mysqlclient" "mysqlclient_r" "mariadbclient"
   HINTS ${HINTS_MYSQL_LIBDIR} ${MYSQL_CONFIG_LIBRARY_PATH}
   PATHS ${PATHS_MYSQL_LIBDIR}
   ${CROSSCOMPILING_NO_CMAKE_SYSTEM_PATH}
 )
-set_extra_dirs_include(MYSQL mysql "${MYSQL_LIBRARY}")
-find_path(MYSQL_INCLUDEDIR
+#######################################
+set_extra_dirs_include(MYSQL mysql "${OWN_MYSQL_LIBRARY}")
+find_path(OWN_MYSQL_INCLUDEDIR
   NAMES "mysql.h"
   HINTS ${HINTS_MYSQL_INCLUDEDIR} ${MYSQL_CONFIG_INCLUDE_DIR}
   PATHS ${PATHS_MYSQL_INCLUDEDIR}
   ${CROSSCOMPILING_NO_CMAKE_SYSTEM_PATH}
 )
 
+####################################### MySQL - CPPCON
 set_extra_dirs_lib(MYSQL_CPPCONN mysql)
-find_library(MYSQL_CPPCONN_LIBRARY
+find_library(OWN_MYSQL_CPPCONN_LIBRARY
   NAMES "mysqlcppconn" "mysqlcppconn-static"
   HINTS ${HINTS_MYSQL_CPPCONN_LIBDIR} ${MYSQL_CONFIG_LIBRARY_PATH}
   PATHS ${PATHS_MYSQL_CPPCONN_LIBDIR}
   ${CROSSCOMPILING_NO_CMAKE_SYSTEM_PATH}
 )
-set_extra_dirs_include(MYSQL_CPPCONN mysql "${MYSQL_CPPCONN_LIBRARY}")
-find_path(MYSQL_CPPCONN_INCLUDEDIR
+#######################################
+set_extra_dirs_include(MYSQL_CPPCONN mysql "${OWN_MYSQL_CPPCONN_LIBRARY}")
+find_path(OWN_MYSQL_CPPCONN_INCLUDEDIR
   NAMES "mysql_connection.h"
   HINTS ${HINTS_MYSQL_CPPCONN_INCLUDEDIR} ${MYSQL_CONFIG_INCLUDE_DIR}
   PATHS ${PATHS_MYSQL_CPPCONN_INCLUDEDIR}
   ${CROSSCOMPILING_NO_CMAKE_SYSTEM_PATH}
 )
 
+#######################################
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(MySQL DEFAULT_MSG MYSQL_LIBRARY MYSQL_INCLUDEDIR)
+find_package_handle_standard_args(MySQL DEFAULT_MSG OWN_MYSQL_LIBRARY OWN_MYSQL_INCLUDEDIR)
 
-if(MYSQL_FOUND)
-  is_bundled(MYSQL_BUNDLED "${MYSQL_LIBRARY}")
+mark_as_advanced(OWN_MYSQL_LIBRARY OWN_MYSQL_INCLUDEDIR)
 
-  set(MYSQL_LIBRARIES ${MYSQL_LIBRARY} ${MYSQL_CPPCONN_LIBRARY})
-  set(MYSQL_INCLUDE_DIRS ${MYSQL_INCLUDEDIR} ${MYSQL_CPPCONN_INCLUDEDIR})
-
-  mark_as_advanced(MYSQL_INCLUDEDIR MYSQL_LIBRARY)
+#######################################
+if(NOT(MYSQL_FOUND))
+  # [some data may not match dependencies / TODO: later fix it]
+  set(CMAKE_MODULE_PATH ${ORIGINAL_CMAKE_MODULE_PATH})
+  find_package(MYSQL)
+  set(CMAKE_MODULE_PATH ${OWN_CMAKE_MODULE_PATH})
+else()
+  set(MYSQL_LIBRARIES ${OWN_MYSQL_LIBRARY} ${OWN_MYSQL_CPPCONN_LIBRARY})
+  set(MYSQL_INCLUDE_DIRS ${OWN_MYSQL_INCLUDEDIR} ${OWN_MYSQL_CPPCONN_INCLUDEDIR})
+  set(MYSQL_COPY_FILES "${EXTRA_MYSQL_LIBDIR}/mysqlcppconn-7-vs14.dll")
 endif()
