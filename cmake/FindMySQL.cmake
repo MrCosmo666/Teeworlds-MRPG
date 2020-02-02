30687e1925026efd29d31366d3acef677fe8601f
vlad <harborwolf1@gmail.com>
2020-02-02 16:14:35 +0300

    ts

--- cmake/FindMySQL.cmake
+++ cmake/FindMySQL.cmake
@@ -1,4 +1,3 @@
-#######################################
 if(NOT CMAKE_CROSSCOMPILING)
   find_program(MYSQL_CONFIG
     NAMES mysql_config mariadb_config
@@ -37,49 +36,44 @@ if(NOT CMAKE_CROSSCOMPILING)
   endif()
 endif()
 
-#######################################
 set_extra_dirs_lib(MYSQL mysql)
-find_library(OWN_MYSQL_LIBRARY
+find_library(MYSQL_LIBRARY
   NAMES "mysqlclient" "mysqlclient_r" "mariadbclient"
   HINTS ${HINTS_MYSQL_LIBDIR} ${MYSQL_CONFIG_LIBRARY_PATH}
   PATHS ${PATHS_MYSQL_LIBDIR}
   ${CROSSCOMPILING_NO_CMAKE_SYSTEM_PATH}
 )
-set_extra_dirs_include(MYSQL mysql "${OWN_MYSQL_LIBRARY}")
-find_path(OWN_MYSQL_INCLUDEDIR
+set_extra_dirs_include(MYSQL mysql "${MYSQL_LIBRARY}")
+find_path(MYSQL_INCLUDEDIR
   NAMES "mysql.h"
   HINTS ${HINTS_MYSQL_INCLUDEDIR} ${MYSQL_CONFIG_INCLUDE_DIR}
   PATHS ${PATHS_MYSQL_INCLUDEDIR}
   ${CROSSCOMPILING_NO_CMAKE_SYSTEM_PATH}
 )
 
-#######################################
 set_extra_dirs_lib(MYSQL_CPPCONN mysql)
-find_library(OWN_MYSQL_CPPCONN_LIBRARY
+find_library(MYSQL_CPPCONN_LIBRARY
   NAMES "mysqlcppconn" "mysqlcppconn-static"
   HINTS ${HINTS_MYSQL_CPPCONN_LIBDIR} ${MYSQL_CONFIG_LIBRARY_PATH}
   PATHS ${PATHS_MYSQL_CPPCONN_LIBDIR}
   ${CROSSCOMPILING_NO_CMAKE_SYSTEM_PATH}
 )
-set_extra_dirs_include(MYSQL_CPPCONN mysql "${OWN_MYSQL_CPPCONN_LIBRARY}")
-find_path(OWN_MYSQL_CPPCONN_INCLUDEDIR
+set_extra_dirs_include(MYSQL_CPPCONN mysql "${MYSQL_CPPCONN_LIBRARY}")
+find_path(MYSQL_CPPCONN_INCLUDEDIR
   NAMES "mysql_connection.h"
   HINTS ${HINTS_MYSQL_CPPCONN_INCLUDEDIR} ${MYSQL_CONFIG_INCLUDE_DIR}
   PATHS ${PATHS_MYSQL_CPPCONN_INCLUDEDIR}
   ${CROSSCOMPILING_NO_CMAKE_SYSTEM_PATH}
 )
 
-#######################################
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(MySQL DEFAULT_MSG OWN_MYSQL_LIBRARY OWN_MYSQL_INCLUDEDIR)
+find_package_handle_standard_args(MySQL DEFAULT_MSG MYSQL_LIBRARY MYSQL_INCLUDEDIR)
 
 if(MYSQL_FOUND)
   is_bundled(MYSQL_BUNDLED "${MYSQL_LIBRARY}")
 
-  set(MYSQL_LIBRARIES ${OWN_MYSQL_LIBRARY} ${OWN_MYSQL_CPPCONN_LIBRARY})
-  set(MYSQL_INCLUDE_DIRS ${OWN_MYSQL_INCLUDEDIR} ${OWN_MYSQL_CPPCONN_INCLUDEDIR})
+  set(MYSQL_LIBRARIES ${MYSQL_LIBRARY} ${MYSQL_CPPCONN_LIBRARY})
+  set(MYSQL_INCLUDE_DIRS ${MYSQL_INCLUDEDIR} ${MYSQL_CPPCONN_INCLUDEDIR})
 
-  mark_as_advanced(OWN_MYSQL_INCLUDEDIR OWN_MYSQL_LIBRARY)
+  mark_as_advanced(MYSQL_INCLUDEDIR MYSQL_LIBRARY)
 endif()
-
-#######################################
