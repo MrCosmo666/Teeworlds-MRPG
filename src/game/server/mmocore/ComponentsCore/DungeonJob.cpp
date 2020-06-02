2a64b16571c49dec64a96b49a0d4a052416c5346
vlad <harborwolf1@gmail.com>
2020-06-02 17:25:11 +0300

    correcting information (Dungeon for allow, need quest)

--- src/game/server/mmocore/ComponentsCore/DungeonJob.cpp
+++ src/game/server/mmocore/ComponentsCore/DungeonJob.cpp
@@ -71,7 +71,7 @@ void DungeonJob::ShowDungeonsList(CPlayer* pPlayer, bool Story)
 		if(NeededQuestID <= 0 || Job()->Quest()->IsCompletedQuest(ClientID, NeededQuestID))
 			GS()->AVM(ClientID, "DUNGEONJOIN", dungeon.first, HideID, "Join dungeon {STR}", dungeon.second.Name);
 		else
-			GS()->AVM(ClientID, "null", NOPE, HideID, "You need to complete {STR}", Job()->Quest()->GetQuestName(NeededQuestID));
+			GS()->AVM(ClientID, "null", NOPE, HideID, "Need to complete quest {STR}", Job()->Quest()->GetQuestName(NeededQuestID));
 	}
 }
 
