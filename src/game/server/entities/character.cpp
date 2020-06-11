42e715c69ee7cdc5f78d285d219020961284a3fe
vlad <harborwolf1@gmail.com>
2020-06-11 13:30:28 +0300

    clearing buff's / die

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -665,6 +665,7 @@ void CCharacter::Die(int Killer, int Weapon)
 	if(Weapon != WEAPON_WORLD && !GS()->IsDungeon())
 	{
 		m_pPlayer->UpdateTempData(0, 0);
+		CGS::Effects[ClientID].clear();
 		const int SafezoneWorldID = GS()->GetRespawnWorld();
 		if(SafezoneWorldID >= 0 && !m_pPlayer->IsBot() && GS()->m_apPlayers[Killer])
 		{
