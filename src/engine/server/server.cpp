527f432f05ecfb91239762d8c80ea1cf96961b5c
vlad <harborwolf1@gmail.com>
2020-07-08 16:17:19 +0300

    some fix discord / info from bans discord bot

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -224,11 +224,18 @@ void CServerBan::ConBanExt(IConsole::IResult *pResult, void *pUser)
 
 	if(!str_is_number(pStr))
 	{
-		int ClientID = str_toint(pStr);
+		const int ClientID = str_toint(pStr);
 		if(ClientID < 0 || ClientID >= MAX_PLAYERS || pThis->Server()->m_aClients[ClientID].m_State == CServer::CClient::STATE_EMPTY)
 			pThis->Console()->Print(IConsole::OUTPUT_LEVEL_STANDARD, "net_ban", "ban error (invalid client id)");
 		else
+		{
+			char aBuf[128];
+			char aAddrStr[NETADDR_MAXSTRSIZE];
+			net_addr_str(pThis->Server()->m_NetServer.ClientAddr(ClientID), aAddrStr, sizeof(aAddrStr), true);
+			str_format(aBuf, sizeof(aBuf), "Player %s IP(%s). Banned for %d minutes!", pThis->Server()->ClientName(ClientID), aAddrStr, Minutes);
+			pThis->Server()->SendDiscordMessage(g_Config.m_SvDiscordAdminChanal, DC_SERVER_WARNING, pThis->Server()->ClientName(ClientID), aBuf);
 			pThis->BanAddr(pThis->Server()->m_NetServer.ClientAddr(ClientID), Minutes*60, pReason);
+		}
 	}
 	else
 		ConBan(pResult, pUser);
