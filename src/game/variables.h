40a52ac3980202c45efdc8c54068534166d54c48
MrCosmo <mail@mrcosmo.info>
2020-10-03 20:31:42 +0200

    added some performance rendering options
    cause I've added the window minimized function, we can stop rendering when the client is minimized or inactive which will decrease the cpu and gpu load to nearly 0

--- src/game/variables.h
+++ src/game/variables.h
@@ -185,6 +185,9 @@ MACRO_CONFIG_INT(SvLoltextVspace, sv_loltext_vspace, 7, 7, 25, CFGFLAG_SERVER, "
 // ui
 MACRO_CONFIG_INT(ClNotifyWindow, cl_notify_window, 1, 0, 1, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Allow client to notify you on chat highlights")
 
+// performance
+MACRO_CONFIG_INT(ClInactiveRendering, cl_inactive_rendering, 1, 0, 2, CFGFLAG_CLIENT, "0 = Always render, 1 = Stop rendering when minimized, 2 = Stop rendering when window is inactive")
+
 // debug
 #ifdef CONF_DEBUG // this one can crash the server if not used correctly
 MACRO_CONFIG_INT(DbgDummies, dbg_dummies, 0, 0, MAX_CLIENTS - 1, CFGFLAG_SERVER, "")
