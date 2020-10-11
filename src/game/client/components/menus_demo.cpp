951a3ef34fb8664c0f848f2fe7433dd0e3065f20
kurosio <harborwolf1@gmail.com>
2020-10-11 18:10:59 +0300

    TW: Demo browser: sort by length

--- src/game/client/components/menus_demo.cpp
+++ src/game/client/components/menus_demo.cpp
@@ -22,9 +22,22 @@
 
 CMenus::CColumn CMenus::ms_aDemoCols[] = {
 	{COL_DEMO_ICON,		-1, " ", -1, 0, 0, {0}, {0}, CUI::ALIGN_CENTER},
-	{COL_DEMO_NAME,		CMenus::SORT_DEMONAME, Localize("Name"), 0, 200.0f, 0, {0}, {0}, CUI::ALIGN_CENTER},
+	{COL_DEMO_NAME,		CMenus::SORT_DEMONAME, Localize("Name"), 0, 100.0f, 0, {0}, {0}, CUI::ALIGN_CENTER},
+	{COL_DEMO_LENGTH,	CMenus::SORT_LENGTH, Localize("Length"), 1, 100.0f, 0, {0}, {0}, CUI::ALIGN_CENTER},
 	{COL_DEMO_DATE,		CMenus::SORT_DATE, Localize("Date"), 1, 160.0f, 0, {0}, {0}, CUI::ALIGN_CENTER},
 };
+
+bool CMenus::FetchHeader(CDemoItem& Item)
+{
+	if(!Item.m_InfosLoaded)
+	{
+		char aBuffer[IO_MAX_PATH_LENGTH];
+		str_format(aBuffer, sizeof(aBuffer), "%s/%s", m_aCurrentDemoFolder, Item.m_aFilename);
+		Item.m_Valid = DemoPlayer()->GetDemoInfo(Storage(), aBuffer, Item.m_StorageType, &Item.m_Info);
+		Item.m_InfosLoaded = true;
+	}
+	return Item.m_Valid;
+}
 
 void CMenus::RenderDemoPlayer(CUIRect MainView)
 {
@@ -425,25 +438,15 @@ void CMenus::RenderDemoList(CUIRect MainView)
 	char aFooterLabel[128] = {0};
 	if(m_DemolistSelectedIndex >= 0)
 	{
-		CDemoItem *Item = &m_lDemos[m_DemolistSelectedIndex];
-		if(str_comp(Item->m_aFilename, "..") == 0)
+		CDemoItem& Item = m_lDemos[m_DemolistSelectedIndex];
+		if(str_comp(Item.m_aFilename, "..") == 0)
 			str_copy(aFooterLabel, Localize("Parent Folder"), sizeof(aFooterLabel));
 		else if(m_DemolistSelectedIsDir)
 			str_copy(aFooterLabel, Localize("Folder"), sizeof(aFooterLabel));
+		else if(!FetchHeader(Item))
+			str_copy(aFooterLabel, Localize("Invalid Demo"), sizeof(aFooterLabel));
 		else
-		{
-			if(!Item->m_InfosLoaded)
-			{
-				char aBuffer[512];
-				str_format(aBuffer, sizeof(aBuffer), "%s/%s", m_aCurrentDemoFolder, Item->m_aFilename);
-				Item->m_Valid = DemoPlayer()->GetDemoInfo(Storage(), aBuffer, Item->m_StorageType, &Item->m_Info);
-				Item->m_InfosLoaded = true;
-			}
-			if(!Item->m_Valid)
-				str_copy(aFooterLabel, Localize("Invalid Demo"), sizeof(aFooterLabel));
-			else
-				str_copy(aFooterLabel, Localize("Demo details"), sizeof(aFooterLabel));
-		}
+			str_copy(aFooterLabel, Localize("Demo details"), sizeof(aFooterLabel));
 	}
 
 	static bool s_DemoDetailsActive = true;
@@ -509,7 +512,10 @@ void CMenus::RenderDemoList(CUIRect MainView)
 				g_Config.m_BrDemoSort = ms_aDemoCols[i].m_Sort;
 			}
 
-			DemolistPopulate();
+			// Don't rescan in order to keep fetched headers, just resort
+			m_lDemos.sort_range_by(CDemoComparator(
+				g_Config.m_BrDemoSort, g_Config.m_BrDemoSortOrder
+			));
 			DemolistOnUpdate(false);
 		}
 	}
@@ -579,13 +585,27 @@ void CMenus::RenderDemoList(CUIRect MainView)
 	}
 
 	// demo buttons
-	int NumButtons = m_DemolistSelectedIsDir ? 2 : 4;
+	int NumButtons = m_DemolistSelectedIsDir ? 3 : 5;
 	float Spacing = 3.0f;
 	float ButtonWidth = (BottomView.w/6.0f)-(Spacing*5.0)/6.0f;
 	float BackgroundWidth = ButtonWidth*(float)NumButtons+(float)(NumButtons-1)*Spacing;
 
 	BottomView.VSplitRight(BackgroundWidth, 0, &BottomView);
 	RenderBackgroundShadow(&BottomView, true);
+
+	BottomView.VSplitLeft(Spacing, 0, &BottomView);
+	BottomView.VSplitLeft(ButtonWidth, &Button, &BottomView);
+	static CButtonContainer s_FetchButton;
+	if(DoButton_Menu(&s_FetchButton, Localize("Fetch Info"), 0, &Button))
+	{
+		for(sorted_array<CDemoItem>::range r = m_lDemos.all(); !r.empty(); r.pop_front())
+		{
+			FetchHeader(r.front());
+		}
+		m_lDemos.sort_range_by(CDemoComparator(
+			g_Config.m_BrDemoSort, g_Config.m_BrDemoSortOrder
+		));
+	}
 
 	BottomView.HSplitTop(25.0f, &BottomView, 0);
 	BottomView.VSplitLeft(ButtonWidth, &Button, &BottomView);
@@ -701,8 +721,7 @@ float CMenus::RenderDemoDetails(CUIRect View)
 
 		View.HSplitTop(Spacing, 0, &View);
 		View.HSplitTop(ButtonHeight, &Button, &View);
-		int Length = ((m_lDemos[m_DemolistSelectedIndex].m_Info.m_aLength[0] << 24) & 0xFF000000) | ((m_lDemos[m_DemolistSelectedIndex].m_Info.m_aLength[1] << 16) & 0xFF0000) |
-			((m_lDemos[m_DemolistSelectedIndex].m_Info.m_aLength[2] << 8) & 0xFF00) | (m_lDemos[m_DemolistSelectedIndex].m_Info.m_aLength[3] & 0xFF);
+		int Length = m_lDemos[m_DemolistSelectedIndex].Length();
 		char aBuf[64];
 		str_format(aBuf, sizeof(aBuf), "%d:%02d", Length/60, Length%60);
 		DoInfoBox(&Button, Localize("Length"), aBuf);
