540fad7eb6d44b0fe61456eb9c6dab712be4d2bf
kurosio <harborwolf1@gmail.com>
2020-11-04 14:46:51 +0300

    mised

--- src/game/server/entities/botai/character_bot_ai.cpp
+++ src/game/server/entities/botai/character_bot_ai.cpp
@@ -171,7 +171,7 @@ void CCharacterBotAI::RewardPlayer(CPlayer* pPlayer, vec2 Force)
 
 		const float RandomDrop = clamp(BotJob::ms_aMobBot[SubID].m_aRandomItem[i] + ActiveLuckyDrop, 0.0f, 100.0f);
 		vec2 ForceRandom(centrelized_frandom(Force.x, Force.x / 4.0f), centrelized_frandom(Force.y, Force.y / 8.0f));
-		GS()->CreateRandomDropItem(m_Core.m_Pos, ClientID, 100, DropItem, ForceRandom);
+		GS()->CreateRandomDropItem(m_Core.m_Pos, ClientID, RandomDrop, DropItem, ForceRandom);
 	}
 
 	// skill point
