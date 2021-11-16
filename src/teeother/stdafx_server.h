7d75acbf0656e6ee3e5fa22546fe3200fb332364
kurosio <harborwolf1@gmail.com>
2021-11-16 11:49:07 +0700

    add pch

--- src/teeother/stdafx_server.h
+++ src/teeother/stdafx_server.h
@@ -12,12 +12,15 @@
 #include <map>
 #include <unordered_map>
 #include <string>
+#include <functional>
+#include <memory>
 
-// custom
+// custom something that is subject to less changes is introduced
 #include <base/vmath.h>
 #include <base/threadpool.h>
 #include <generated/protocol.h>
 #include <game/game_context.h>
 #include <teeother/tl/nlohmann_json.h>
+#include <teeother/components/localization.h>
 
 #endif //SERVER_STDAFX_H
